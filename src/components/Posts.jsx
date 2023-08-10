488eef65878efeaa8030af8a186dc0b2b35fceca
Misty <mistybarnes79@gmail.com>
2023-08-10 19:26:53 -0400

    post

---
+++ src/components/Posts.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { fetchAllPosts } from "../api";
+import Post from "./components/Post";
+
+export default function Posts(){
+
+    const [posts, setPosts] = useState([]);
+
+    /* fetch posts using api file function fetchAllPosts*/
+    useEffect(() => {
+      const getAllPosts = async () => {
+        try {
+          const result = await fetchAllPosts();
+          setPosts(result);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      getAllPosts();
+    }, []);
+    return (
+
+    <div className="post-container">
+      {/* loops through each post object and displays its data in Post.jsx component */}
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post._id}
+            username={post.author.username}
+            title={post.title}
+            description={post.description}
+            price={post.price}
+          />
+        );
+      })}
+    </div>
+  );
+}
