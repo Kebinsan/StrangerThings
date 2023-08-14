05123df09bc6e1d6ef912295492c6fa64f36440b
rashad3412 <rashd3412@gmail.com>
2023-08-14 19:32:44 -0400

    updated register

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,20 +3,6 @@ import { fetchAllPosts } from "../api";
 import Post from "./Post";
 
 export default function Posts() {
-  const [posts, setPosts] = useState([]);
-
-  /* fetch posts using api file function fetchAllPosts*/
-  useEffect(() => {
-    const getAllPosts = async () => {
-      try {
-        const result = await fetchAllPosts();
-        setPosts(result);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getAllPosts();
-  }, []);
   return (
     <div className="post-container">
       {/* loops through each post object and displays its data in Post.jsx component */}
